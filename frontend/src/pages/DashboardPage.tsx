e54f44c61df2eb6ae60ac4c40594971ff89ae4a9
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-05-25 19:19:12 -0300

    add page background to dashboard page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,15 @@
+import { PageBackground } from "../components/layout/PageBackground";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <PageBackground>
+      <div className="w-full max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+        <h1 className="text-3xl font-bold text-onyx dark:text-timberwolf mb-4">
+          Dashboard
+        </h1>
+      </div>
+    </PageBackground>
+  );
 };
 
-// DashboardPage component for the dashboard page
 export default DashboardPage;
